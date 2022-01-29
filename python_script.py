9ca7dc0b3f6fb3936387a8f8ba1b3d7107800e9c
Shotparis <98637127+Shotparis@users.noreply.github.com>
2022-01-29 16:43:09 +0300

    Ex6

--- python_script.py
+++ python_script.py
@@ -2,7 +2,10 @@ import json
 
 json_text = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
 obj = json.loads(json_text)
-print(obj["messages"])
 
+key = "messages"
 
-
+if key in obj:
+    print(obj[key][1])
+else:
+    print(f"Ключа {key} в JSON нет")
